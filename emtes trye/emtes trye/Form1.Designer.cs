504006fbbf662c1d642faff5d9c14a5b786de40f
diego <diego_ch231@hotmail.com>
2015-02-19 03:41:26 -0600

    iconos program
    pene

---
+++ emtes trye/emtes trye/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace emtes_trye
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(398, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 24);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(461, 128);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.TabStop = false;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 179);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(461, 96);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TabStop = false;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 347);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ emtes trye/emtes trye/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace emtes_trye
+{
+    
+    public partial class Form1 : Form
+    {
+        //bool send;
+        string textsend;
+        Hashtable emotions;
+        
+        void createicons()
+        {
+            emotions = new Hashtable(1);
+            emotions.Add(":)", emtes_trye.Properties.Resources.smile);
+            emotions.Add("Kappa", emtes_trye.Properties.Resources.kappa);
+            emotions.Add("Pink", emtes_trye.Properties.Resources.pink);
+            emotions.Add("Fail", emtes_trye.Properties.Resources.fail);
+            emotions.Add("T.T", emtes_trye.Properties.Resources.cry);
+            emotions.Add("^<^", emtes_trye.Properties.Resources.smush);
+            emotions.Add("<3.<3", emtes_trye.Properties.Resources.love);
+            emotions.Add("O.O", emtes_trye.Properties.Resources.wow);
+            emotions.Add(":D", emtes_trye.Properties.Resources.happy);
+            emotions.Add("¬.¬", emtes_trye.Properties.Resources.me);
+            emotions.Add("?-?", emtes_trye.Properties.Resources.what);
+            emotions.Add(";)", emtes_trye.Properties.Resources.wink);
+            emotions.Add(":'(", emtes_trye.Properties.Resources.sad);
+            emotions.Add(">_<", emtes_trye.Properties.Resources.scream);
+            emotions.Add("<3", emtes_trye.Properties.Resources.heart);
+            emotions.Add(":#X", emtes_trye.Properties.Resources.dead);
+            emotions.Add("Hambre", emtes_trye.Properties.Resources.hungry);
+            emotions.Add("Zzz", emtes_trye.Properties.Resources.boring);
+            emotions.Add("Like", emtes_trye.Properties.Resources.like);
+           
+
+
+        }
+
+        void pegaricono ()
+        {
+            richTextBox2.AppendText(textBox2.Text+"\n");
+            foreach (String emote in emotions.Keys)
+            {
+                while (richTextBox2.Text.Contains(emote))
+                {
+                    
+                   int ind = richTextBox2.Text.IndexOf(emote);
+                    richTextBox2.Select(ind,emote.Length);
+                    Clipboard.SetImage((Image)emotions[emote]);
+                    richTextBox2.Paste();
+
+                    textBox2.Clear();
+                
+                }
+            }
+        }
+
+        void pasatexto()
+        {
+            Clipboard.SetText(richTextBox2.Rtf, TextDataFormat.Rtf);
+
+            textBox2.Select(richTextBox2.TextLength - 1, 1);
+
+        
+        }
+    
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            createicons();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            pegaricono();
+            textBox2.Clear();
+            
+            //string richText = new TextRange(richTextBox2.Document.ContentStart, richTextBox2.Document.ContentEnd).Text;
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+            
+       
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            //textsend = textBox2.Text;
+        }
+       }
+    }
+
