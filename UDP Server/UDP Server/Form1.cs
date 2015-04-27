3becc4231d25495e077993d95dcc7e2900121c42
monotr <joseluis.gonf@gmail.com>
2015-04-27 15:02:08 -0500

    other check

--- UDP Server/UDP Server/Form1.cs
+++ UDP Server/UDP Server/Form1.cs
@@ -58,7 +58,7 @@ namespace UDP_Server
                     }
                 }
 
-                
+                conectionsList.Items.Add(RemoteIpEndPoint.Address.ToString());
             }
         }
 
